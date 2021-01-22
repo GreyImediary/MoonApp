9c96d0d5721de7ed2375dd52efc1dbebca3d4cd2
GreyImediary <ser.kushnaryov@gmail.com>
2021-01-22 18:17:08 +0300

    rename member for more precision

--- app/src/main/java/com/example/moonapp/api/MoonService.kt
+++ app/src/main/java/com/example/moonapp/api/MoonService.kt
@@ -6,5 +6,5 @@ import retrofit2.http.Query
 
 interface MoonService {
     @GET("forecast?aggregateHours=24&includeAstronomy=true&combinationMethod=aggregate&contentType=json&unitGroup=metric&locationMode=single&key=E1QNE6L2XN5LQ849XB8R15ZQ4")
-    suspend fun getMoonPhase(@Query("locations") latLng: String): MoonDayData
+    suspend fun getMoonPhase(@Query("locations") timeZone: String): MoonDayData
 }
