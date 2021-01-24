71e8a4adc6d642036eff083b6a0bca16fbed2115
GreyImediary <ser.kushnaryov@gmail.com>
2021-01-24 19:40:41 +0300

    fix back button on subscription page

--- app/src/main/java/com/example/moonapp/ui/SubscriptionFragment.kt
+++ app/src/main/java/com/example/moonapp/ui/SubscriptionFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.moonapp.databinding.FragmentSubscriptionBinding
 
 /**
@@ -21,5 +23,13 @@ class SubscriptionFragment : Fragment() {
     ): View {
         _binding = FragmentSubscriptionBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        requireActivity().onBackPressedDispatcher.addCallback {
+            findNavController().popBackStack()
+        }
     }
 }
