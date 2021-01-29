e8300dcd1dbe7fd8979cc365df8992758c0d7a36
GreyImediary <ser.kushnaryov@gmail.com>
2021-01-29 18:13:50 +0300

    fix moon day calculation: use astrology site parsing instead of weather api

--- app/src/main/java/com/example/moonapp/extensions.kt
+++ app/src/main/java/com/example/moonapp/extensions.kt
@@ -3,6 +3,12 @@ package com.example.moonapp
 import android.content.Context
 import android.content.SharedPreferences
 import android.view.View
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.Response
+import java.io.IOException
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 fun View.gone() {
     visibility = View.GONE
@@ -18,3 +24,19 @@ fun View.invisible() {
 
 val Context.prefs: SharedPreferences
     get() = getSharedPreferences("MOON_SHARED_PREFS", Context.MODE_PRIVATE)
+
+suspend fun getHttpCallBody(call: Call) = suspendCoroutine<String> {
+    call.enqueue(object : Callback {
+        override fun onFailure(call: Call, e: IOException) {
+            throw e
+        }
+
+        override fun onResponse(call: Call, response: Response) {
+            if (response.isSuccessful) {
+                it.resume(response.body()!!.string())
+            } else {
+                it.resume("")
+            }
+        }
+    })
+}
--- app/src/main/java/com/example/moonapp/viewModels/MainScreenViewModel.kt
+++ app/src/main/java/com/example/moonapp/viewModels/MainScreenViewModel.kt
@@ -3,11 +3,9 @@ package com.example.moonapp.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import com.example.moonapp.api.MoonService
-import com.example.moonapp.data.MoonDayData
 import com.example.moonapp.data.Result
-import java.util.*
-import kotlin.math.ceil
-import kotlin.math.floor
+import com.example.moonapp.getHttpCallBody
+import okhttp3.*
 
 class MainScreenViewModel(private val api: MoonService) : ViewModel() {
 
@@ -28,18 +26,44 @@ class MainScreenViewModel(private val api: MoonService) : ViewModel() {
      * Возвращает числовое значение лунного дня, преобразовывая double с диапазоном 0.0-1.0
      * в Int с диапазоном 1-30
      * */
-    private suspend fun getMoonDay(): Int {
-        val timeZoneId: String = TimeZone.getDefault().id
-        val moonDayData: MoonDayData = api.getMoonPhase(timeZoneId)
-        val moonPhase: Double = moonDayData.moonDayLocation.currentCondition.moonPhase
 
-        return when {
+    /*
+    * */
+    private suspend fun getMoonDay(): Int {
+        /*val timeZoneId: String = TimeZone.getDefault().id
+        val moonDayData: MoonDayData = api.getMoonPhase(timeZoneId)
+        val moonPhase: Double = moonDayData.moonDayLocation.currentCondition.moonPhase*/
+
+
+        val http = OkHttpClient()
+        val request = Request.Builder()
+            .url("https://mirkosmosa.ru/lunar-calendar")
+            .build()
+
+        val response = getHttpCallBody(http.newCall(request))
+
+
+
+        /*return when {
             moonPhase - moonPhase.toInt() < 0.1 -> ceil(moonPhase).toInt()
             moonPhase - moonPhase.toInt() < 0.5 -> floor(moonPhase * 30).toInt()
             moonPhase - moonPhase.toInt() > 0.5 -> ceil(moonPhase * 30).toInt()
             moonPhase == 0.0 -> 1
             else -> (moonPhase * 30).toInt()
 
-        }
+        }*/
+
+        return parseMoonDay(response)
+    }
+
+    private fun parseMoonDay(htmlBody: String): Int {
+        val classIndex = htmlBody.indexOf("moon-age_sym") // находим индекс класса тега, в котором находится день
+        val sub = htmlBody.substring(classIndex) // отсекаем лишнее
+
+        // вовзращаем значение лунного дня
+        return sub.substring(
+            sub.indexOf(">" ) + 1,
+            sub.indexOf("<")
+        ).toInt()
     }
 }
